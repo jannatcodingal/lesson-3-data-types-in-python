c2d7f49a9a78281df00cede108093daba36950ca
jannatcodingal <jannatrehman007@gmail.com>
2025-03-07 21:20:03 +0500

    type casting

---
+++ type casting.py
@@ -0,0 +1,20 @@
+name="penguin"
+age=15
+is_student=True
+weight=38.5
+#printing different variables and their types
+print("name: ", name)
+print("data type of name is ", type(name))
+print("age:", age)
+print("Data type of age is",type(age))
+print("is_student:, is_student")
+print("data type of is_student is",type(is_student))
+print("weight:",weight)
+print("data type of weight is",type(weight))
+#type casting to convert the data type of variables
+print("in after type casting...")
+age=str(age)
+print("data type of age is",type(age))
+weight=int(weight)
+print(weight)
+
